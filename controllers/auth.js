5aab1478cc58096802702a92ff1076e339eb64a9
Wesley Patterson <wpatt849@gmail.com>
2022-09-05 19:35:23 -0500

    Added async function to auth.js/edit.js in controllers and to passport.js in config. Also added callback function to req.logout in auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,7 +37,9 @@ const User = require('../models/User')
   }
   
   exports.logout = (req, res) => {
-    req.logout()
+    req.logout(() => {
+      console.log('User has logged out.')
+    })
     req.session.destroy((err) => {
       if (err) console.log('Error : Failed to destroy the session during logout.', err)
       req.user = null
@@ -54,7 +56,7 @@ const User = require('../models/User')
     })
   }
   
-  exports.postSignup = (req, res, next) => {
+  exports.postSignup = async (req, res, next) => {
     const validationErrors = []
     if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
     if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
@@ -72,7 +74,7 @@ const User = require('../models/User')
       password: req.body.password
     })
   
-    User.findOne({$or: [
+    await User.findOne({$or: [
       {email: req.body.email},
       {userName: req.body.userName}
     ]}, (err, existingUser) => {
